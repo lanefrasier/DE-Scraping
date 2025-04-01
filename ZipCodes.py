fa4a7b519a1369d9546f707127da80db74d51bf2
Lanefrasier <lanefrasier22@gmail.com>
2025-04-01 15:16:32 -0500

    first run

--- ZipCodes.py
+++ ZipCodes.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # Define file paths and sheet names
 input_file = "InputData.xlsx"
-zip_file = "C:/Users/LFrasier/NRG Energy, Inc/Digital Operations-NRG365 - General/Process Documents/USN State Rate Board Documents/USN SS Logins.xlsx"
+zip_file = "USN SS Logins.xlsx"
 sheet_input = "USN_Pricing_Master"
 sheet_zip = "ZipCodes by State"
 
